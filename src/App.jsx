4aa38c8db17d6d53457665a6364d811821caa6bc
Arthur Cabral <arthurcabral.dev@gmail.com>
2021-08-02 18:53:10 -0300

    Creating Functions And Login Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,100 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Olá</h1>
-    </div>
-  );
-}
+import React, {useState, useEffect} from 'react'
+import { GlobalStyle } from './styles/GlobalStyles'
 
-export default App;
+import Login from './components/Login'
+
+import fire from './database/fire';
+
+export default function App() {
+
+  const [user,setUser] = useState('');
+
+  const [email,setEmail] = useState('');
+  const [emailError,setEmailError] = useState('');
+
+  const [password,setPassword] = useState('');
+  const [passwordError,setPasswordError] = useState('');
+
+  const [hasAccount,setHasAccount] = useState(false);
+
+  const clearInputs = () =>{
+    setEmail('');
+    setPassword('');
+  }
+
+  const clearErrors = () =>{
+    setEmailError('');
+    setPasswordError('');
+  }
+
+  const handleLogin = () => {
+    fire.auth()
+    .signInWithEmailAndPassword(email,password)
+    .catch((err) =>{
+      switch(err.code){
+        case 'auth/invalid-email':
+        case 'auth/user-disabled':
+        case 'auth/user-not-found':
+          setEmailError(err.message);
+          break;
+        case 'auth/wrong-password':
+          setPasswordError(err.message);
+          break;
+      }
+
+    })
+  }
+
+  const handleSignup = () => {
+    fire.auth()
+    .createUserWithEmailAndPassword(email,password)
+    .catch((err) =>{
+      switch(err.code){
+        case 'auth/email-already-in-use':
+        case 'auth/invalid-email':
+          setEmailError(err.message);
+          break;
+        case 'auth/weak-password':
+          setPasswordError(err.message);
+          break;
+      }
+    })
+  }
+
+  const handleLogout = () => {
+    fire.auth().signOut()
+  }
+
+  const authListener = () =>{
+    fire.auth().onAuthStateChanged((user) =>{
+      if(user){
+        clearInputs();
+        setUser(user);
+      } else{
+        setUser('')
+      }
+    })
+  }
+
+  useEffect(() =>{
+    authListener();
+  }, [])
+
+  return (
+    <div>
+      <Login 
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      handleLogin={handleLogin}
+      handleSignup={handleSignup}
+      hasAccount={hasAccount}
+      setHasAccount={setHasAccount}
+      emailError={emailError}
+      passwordError={passwordError}
+      />
+      <GlobalStyle/>
+    </div>
+  )
+}
